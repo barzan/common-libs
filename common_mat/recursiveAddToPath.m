03773c81871cd199458ae33054901a9c2844a36b
bm <sina@0587386730.wireless.umich.net>
2014-06-12 17:48:03 -0400

    added a file to recursively add subdirectories to the path

---
+++ common_mat/recursiveAddToPath.m
@@ -0,0 +1,28 @@
+function recursiveAddToPath(topDir, varargin)
+
+%topDir = '.';
+queue = {topDir};
+
+while ~isempty(queue)
+    curDir = queue{1};
+    queue(1) = [];
+    
+    if ~isdir(curDir)
+        continue;
+    end
+    
+    %fprintf(1, '%s\n', curDir);
+    addpath(curDir);
+    newDirs = dir(curDir);
+    for i=1:length(newDirs)
+        d = newDirs(i).name;
+        if strcmp(d, '.') || strcmp(d, '..')
+            continue;
+        end
+        dirToAdd = [curDir '/' d];
+        queue = [queue dirToAdd];
+    end
+end
+
+    disp(['added ' topDir ' to matlab''s path']);
+end
